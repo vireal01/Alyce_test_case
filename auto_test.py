1a85aeb4ebe7f29a47af97a9932e2c9479a308d5
vireal01 <vireal014@yandex.ru>
2020-02-04 08:54:45 +0300

    add fourth test (Check the pop-up message, if you're trying to take an apple from the empty basket)

--- auto_test.py
+++ auto_test.py
@@ -175,7 +175,7 @@ class Apples(unittest.TestCase):
         driver.find_element_by_xpath \
             ('/html/body/div[1]/div[2]/div[1]/div')
 
-    '''Test Three:
+    '''Test Four:
         Check the pop-up message, if you're trying to take an apple from the empty basket'''
     def test_four(self):
         driver = self.driver
